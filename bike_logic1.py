9b6c6ed43a5bbbf9f14477d32d58d2caecbd417b
O-Gee-San <bdgee925@gmail.com>
2023-05-20 22:24:23 -0700

    Update bike_logic1.py

--- bike_logic1.py
+++ bike_logic1.py
@@ -1,6 +1,7 @@
 #First index is leftmost, second Index is middle left, third index is middle right, and fourth index is right
 
 #Sense the button
+from pyfirmata import Arduino
 import pyfirmata
 import time
 
@@ -17,12 +18,11 @@ board = pyfirmata.Arduino('COM3')
 print("Communication Successfully started")
 
 # Create bunch of useful variables
-button = board.analog[1]
+button = board.get_pin('a:1:i')
 LED1 = board.digital[9]
 LED2 = board.digital[10]
 LED3 = board.digital[11]
 LED4 = board.digital[12]
-button.enable_reporting()
 LEDs = [LED1, LED2, LED3, LED4] 
 LED_index = 0
 previous_button_state = 0
@@ -32,32 +32,9 @@ it = pyfirmata.util.Iterator(board)
 it.start()
 
 # Setup LEDs and button
-button.mode = pyfirmata.INPUT
 
 for LED in LEDs:
     LED.write(0)
 
-# The "void loop()"
 while True:
-    # We run the loop at 100Hz
-    time.sleep(0.01)
-    
-    # Get button current state
-    button_state = button.read()
-    
-    # Check if button has been released
-    if button_state != previous_button_state:
-        if button_state == 0:
-            print("Button released")
-
-            # Power off current LED 
-            # and power on next LED
-            LEDs[LED_index].write(0)
-            LED_index += 1
-            if LED_index >= len(LEDs):
-                LED_index = 0
-            LEDs[LED_index].write(1)
-        
-    # Save current button state as previous
-    # for the next loop iteration
-    previous_button_state = button_state
+    print(button.read)
