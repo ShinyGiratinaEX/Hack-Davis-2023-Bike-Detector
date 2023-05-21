06f28017793991294b7757772a608d2823604b03
O-Gee-San <bdgee925@gmail.com>
2023-05-21 00:19:24 -0700

    Update bike_logic1.py

--- bike_logic1.py
+++ bike_logic1.py
@@ -4,8 +4,18 @@
 from pyfirmata import Arduino
 import pyfirmata
 import time
+import takepicture 
+""" servo pyfirmata"""
 
 board = pyfirmata.Arduino('COM3')
+
+iter8 = pyfirmata.util.Iterator(board)
+iter8.start()
+
+global servo_pin
+
+def move_servo(angle):
+    servo_pin.write(angle)
 
 # i = 0
 # while True:
@@ -15,26 +25,25 @@ board = pyfirmata.Arduino('COM3')
 #     time.sleep(1)
 #     i += 1
 
-print("Communication Successfully started")
+# # Create bunch of useful variables
+button = board.analog[0]
 
-# Create bunch of useful variables
-button = board.get_pin('a:1:i')
-LED1 = board.digital[9]
-LED2 = board.digital[10]
-LED3 = board.digital[11]
-LED4 = board.digital[12]
-LEDs = [LED1, LED2, LED3, LED4] 
-LED_index = 0
-previous_button_state = 0
-
-# Start iterator to receive input data
-it = pyfirmata.util.Iterator(board)
-it.start()
-
-# Setup LEDs and button
-
-for LED in LEDs:
-    LED.write(0)
+button.enable_reporting()
 
 while True:
-    print(button.read)
+    print(button.read())
+    try:
+        if button.read() > 0.5:
+            break
+    except:
+        print("Error") 
+
+servo_pin = board.get_pin('d:2:s')
+move_servo(0)
+time.sleep(3)
+# for i in range(1,91):
+#     move_servo(i)
+move_servo(90)
+# takepicture.main(0)
+time.sleep(3)
+move_servo(180)
