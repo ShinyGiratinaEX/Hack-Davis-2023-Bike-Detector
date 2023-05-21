e764a98a4427d2ccc56535986517c97fd3d76d93
O-Gee-San <bdgee925@gmail.com>
2023-05-20 22:55:35 -0700

    Update takepicture.py

--- takepicture.py
+++ takepicture.py
@@ -1,27 +1,16 @@
-import numpy as np  
-import cv2  
-  
-cap = cv2.VideoCapture(0)  
-  
-# Define the codec and create VideoWriter object  
-fourcc = cv2.VideoWriter_fourcc(*'XVID')  
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))  
-  
-while(cap.isOpened()):  
-    ret, frame = cap.read()  
-    if ret==True:  
-        frame = cv2.flip(frame,0)  
-  
-        # write the flipped frame  
-        out.write(frame)  
-  
-        cv2.imshow('frame',frame)  
-        if cv2.waitKey(1) & 0xFF == ord('q'):  
-            break  
-    else:  
-        break  
-  
-# Release everything if job is finished  
-cap.release()  
-out.release()  
-cv2.destroyAllWindows()  
+import cv2
+import time
+
+def main():
+
+    cam = cv2.VideoCapture(0)
+
+    img_counter = 0
+
+    ret, frame = cam.read()
+    img_name = "bike{}.png".format(img_counter)
+    cv2.imwrite(img_name, frame)
+
+    cam.release()
+
+    cv2.destroyAllWindows()
